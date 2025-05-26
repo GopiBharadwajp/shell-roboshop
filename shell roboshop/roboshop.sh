5f0ccd59db067becd23bc1460f95d76b3cfa0f81
gopi bharadwaj <gopipathapalli@gmail.com>
2025-05-26 13:17:25 +0530

    code added

--- shell roboshop/roboshop.sh
+++ shell roboshop/roboshop.sh
@@ -13,11 +13,11 @@ do
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
-        IP=aws ec2 describe-instances --instance-ids i-0abcdef123456789 
-        --query "Reservations[0].Instances[0].PrivateIpAddress" --output tex
+        IP=$(aws ec2 describe-instances --instance-ids i-0abcdef123456789 
+        --query "Reservations[0].Instances[0].PrivateIpAddress" --output tex)
     else
-       IP=aws ec2 describe-instances --instance-ids i-0abcdef123456789 
-        --query "Reservations[0].Instances[0].PublicIpAddress" --output tex
+       IP=$(aws ec2 describe-instances --instance-ids i-0abcdef123456789 
+        --query "Reservations[0].Instances[0].PublicIpAddress" --output tex)
     fi
-   
+   echo "$INstance IP adress: $IP"
     done
