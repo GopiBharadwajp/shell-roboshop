2f3c9a30e904180a2cbcd50218be3a521c0e66ed
gopi bharadwaj <gopipathapalli@gmail.com>
2025-05-27 15:47:08 +0530

    code added

--- shell roboshop/roboshop.sh
+++ shell roboshop/roboshop.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
+
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-03a031274602046a1"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z0095891220EQ8FJXWFW1"
 DOMAINNAME="gorobo.site"
 
-for instance in ${INSTANCES[@]}
-
+for instance in "${INSTANCES[@]}"
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro 
-    --security-group-ids sg-03a031274602046a1 --tag-specifications "ResourceType=instance,
-    Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != "frontend" ]
-    then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --instance-type t2.micro \
+        --security-group-ids $SG_ID \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query "Instances[0].InstanceId" \
+        --output text)
+
+    if [ "$instance" != "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query "Reservations[0].Instances[0].PrivateIpAddress" \
+            --output text)
     else
-       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query "Reservations[0].Instances[0].PublicIpAddress" \
+            --output text)
     fi
 
     echo "$instance IP address: $IP"
-    done
+done
